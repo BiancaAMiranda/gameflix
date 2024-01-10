129dcf17d4d1477ffb7f0322cd16375e240349dc
BiancaAMiranda <bianca.amirandaaguiar@gmail.com>
2024-01-10 18:58:32 -0300

    click x

---
+++ docs/noughts_crosses_game/noughts_crosses.js
@@ -0,0 +1,58 @@
+
+const first_area = document.getElementById('first_area');
+const second_area = document.getElementById('second_area');
+const third_area = document.getElementById('third_area');
+const fourth_area = document.getElementById('fourth_area');
+const fifth_area = document.getElementById('fifth_area');
+const sixth_area = document.getElementById('sixth_area');
+const seventh_area = document.getElementById('seventh_area');
+const eighth_area = document.getElementById('eighth_area');
+const nineth_area = document.getElementById('nineth_area');
+
+first_area.onclick = () => {
+    //document.getElementById('first_area').innerHTML('<p>x</p>')
+    let element = document.getElementById('first_area');
+    element.innerHTML = 'x'
+    element.style.maxWidthwidth = 'calc(500/3px)'
+}
+
+second_area.onclick = () => {
+    //document.getElementById('first_area').innerHTML('<p>x</p>')
+    let element = document.getElementById('second_area');
+    element.innerHTML = 'x'
+}
+third_area.onclick = () => {
+    //document.getElementById('first_area').innerHTML('<p>x</p>')
+    let element = document.getElementById('third_area');
+    element.innerHTML = 'x'
+}
+fourth_area.onclick = () => {
+    //document.getElementById('first_area').innerHTML('<p>x</p>')
+    let element = document.getElementById('fourth_area');
+    element.innerHTML = 'x'
+}
+fifth_area.onclick = () => {
+    //document.getElementById('first_area').innerHTML('<p>x</p>')
+    let element = document.getElementById('fifth_area');
+    element.innerHTML = 'x'
+}
+sixth_area.onclick = () => {
+    //document.getElementById('first_area').innerHTML('<p>x</p>')
+    let element = document.getElementById('sixth_area');
+    element.innerHTML = 'x'
+}
+seventh_area.onclick = () => {
+    //document.getElementById('first_area').innerHTML('<p>x</p>')
+    let element = document.getElementById('seventh_area');
+    element.innerHTML = 'x'
+}
+eighth_area.onclick = () => {
+    //document.getElementById('first_area').innerHTML('<p>x</p>')
+    let element = document.getElementById('eighth_area');
+    element.innerHTML = 'x'
+}
+nineth_area.onclick = () => {
+    //document.getElementById('first_area').innerHTML('<p>x</p>')
+    let element = document.getElementById('nineth_area');
+    element.innerHTML = 'x'
+}
